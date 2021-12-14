831eef0207d1bef9de9b56e19287087255d24751
Алексей Бельченков <u608110@gmail.com>
2021-12-14 19:58:34 +0400

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Header from './components/Header';
+
 function App() {
     return (
-        <h1>Hello, Feedback!</h1>
+        <>
+            <Header text="Feedback UI" />
+            <div className="container">
+                <h1>App!</h1>
+            </div>
+        </>
     );
 }
 
