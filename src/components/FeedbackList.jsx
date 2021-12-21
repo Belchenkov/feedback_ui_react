a71116ebc5a76332158a7219c1de15d64562d075
Алексей Бельченков <u608110@gmail.com>
2021-12-21 12:00:31 +0400

    Card Component & Conditional Styles

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import FeedbackItem from './FeedbackItem';
 
 const FeedbackList = ({ feedback }) => {
@@ -19,5 +21,15 @@ const FeedbackList = ({ feedback }) => {
         </div>
     );
 };
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+}
 
 export default FeedbackList;
