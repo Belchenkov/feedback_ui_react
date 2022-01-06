271a974472b738d7962c6daf484f202b3d5e80d2
Алексей Бельченков <u608110@gmail.com>
2022-01-06 17:00:12 +0400

    Router & About Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
@@ -25,21 +27,24 @@ function App() {
     };
 
     return (
-        <>
+        <Router>
             <Header text="Feedback UI" />
             <div className="container">
-                <FeedbackForm
-                    handleAdd={addFeedback}
-                />
-                <FeedbackStats
-                    feedback={feedback}
-                />
-                <FeedbackList
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                />
+                <Route path='/' exact>
+                    <FeedbackForm
+                        handleAdd={addFeedback}
+                    />
+                    <FeedbackStats
+                        feedback={feedback}
+                    />
+                    <FeedbackList
+                        feedback={feedback}
+                        handleDelete={deleteFeedback}
+                    />
+                </Route>
+                <Route path='/about' component={AboutPage} />
             </div>
-        </>
+        </Router>
     );
 }
 
