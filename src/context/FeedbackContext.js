3189f1bec9aaef9a1a0163296200ea19f7e63937
Алексей Бельченков <u608110@gmail.com>
2022-01-18 11:52:23 +0400

    Update & Delete feedback throw server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -44,12 +44,25 @@ export const FeedbackProvider = ({ children }) => {
       })
     };
 
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map(item => item.id === id ? { ...item, ...updItem } : item));
+    const updateFeedback = async (id, updItem) => {
+        const res = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem),
+        });
+        const data = await res.json();
+
+        setFeedback(feedback.map(item => item.id === id ? { ...item, ...data } : item));
     };
 
-    const deleteFeedback = id => {
+    const deleteFeedback = async id => {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`/feedback/${id}`, {
+                method: 'DELETE',
+            });
+
             setFeedback(feedback.filter((item) => item.id !== id));
         }
     };
